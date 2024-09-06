62504b427b91c7bbc784e47852aa64dc15800c68
Armas <Armas.Nevolainen@metropolia.fi>
2024-09-06 15:15:12 +0300

    Draft of Available tools page

---
+++ project_fe/src/components/Pagination.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Pagination({ currentPage, totalPages, onPageChange }) {
+  const pageNumbers = [];
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div className="pagination">
+      <button
+        onClick={() => onPageChange(Math.max(currentPage - 1, 1))}
+        disabled={currentPage === 1}
+      >
+        Previous
+      </button>
+      {pageNumbers.map((number) => (
+        <button
+          key={number}
+          onClick={() => onPageChange(number)}
+          className={currentPage === number ? "active" : ""}
+        >
+          {number}
+        </button>
+      ))}
+      <button
+        onClick={() => onPageChange(Math.min(currentPage + 1, totalPages))}
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
